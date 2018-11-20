3156cac94136db38017d0d8e41d5ed2b53a0c56b
Jean-Michel Lehker <j.lehker@gmail.com>
2018-11-19 21:38:04 -0800

    add mailgun and migrations

---
+++ bestlists/core/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from model_utils.models import TimeStampedModel
+
+from bestlists.users.models import User
+
+
+class MasterList(TimeStampedModel):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class TodoList(TimeStampedModel):
+    master_list = models.ForeignKey(MasterList, on_delete=models.CASCADE)
+    name = models.TextField()
+
+
+class ListItem:
+    todo_list = models.ForeignKey(TodoList, on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+    due_date = models.DateField()
--- config/settings/production.py
+++ config/settings/production.py
@@ -122,10 +122,10 @@ ADMIN_URL = env('DJANGO_ADMIN_URL')
 INSTALLED_APPS += ['anymail']  # noqa F405
 EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
-#ANYMAIL = {
-#    'MAILGUN_API_KEY': env('MAILGUN_API_KEY'),
-#    'MAILGUN_SENDER_DOMAIN': env('MAILGUN_DOMAIN')
-#}
+ANYMAIL = {
+   'MAILGUN_API_KEY': env('MAILGUN_API_KEY'),
+   'MAILGUN_SENDER_DOMAIN': env('MAILGUN_DOMAIN')
+}
 
 # Gunicorn
 # ------------------------------------------------------------------------------
