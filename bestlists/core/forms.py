8b9b76cb97b356d45382671068f4dd895f0b2bd7
Jean-Michel Lehker <j.lehker@gmail.com>
2018-11-22 23:55:00 -0800

    add some UI elements and font awesome icons

--- bestlists/core/forms.py
+++ bestlists/core/forms.py
@@ -6,6 +6,10 @@ from bestlists.core.models import ListItem
 class TodoItemForm(forms.ModelForm):
     description = forms.CharField(label="Description", max_length=100)
 
+    def __init__(self, *args, **kwargs):
+        super(TodoItemForm, self).__init__(*args, **kwargs)
+        self.fields["description"].widget.attrs.update({"autofocus": ""})
+
     class Meta:
         model = ListItem
         fields = ("description",)
