ea2bb1cf093887519f2d48e990e2738ba5925c8d
Jean-Michel Lehker <j.lehker@gmail.com>
2018-12-03 23:57:27 -0800

    fix tests

--- todovoodoo/core/tests/test_urls.py
+++ todovoodoo/core/tests/test_urls.py
@@ -10,21 +10,21 @@ def test_delete_list():
     pub_id = uuid.uuid4()
     assert (
         reverse("core:delete-todo-list", kwargs={"pub_id": str(pub_id)})
-        == f"/app/lists/delete/{pub_id}/"
+        == f"/v0/lists/delete/{pub_id}/"
     )
-    assert resolve(f"/app/lists/delete/{pub_id}/").view_name == "core:delete-todo-list"
+    assert resolve(f"/v0/lists/delete/{pub_id}/").view_name == "core:delete-todo-list"
 
 
 def test_create_list():
-    assert reverse("core:create-todo-list") == "/app/lists/create/"
-    assert resolve("/app/lists/create/").view_name == "core:create-todo-list"
+    assert reverse("core:create-todo-list") == "/v0/lists/create/"
+    assert resolve("/v0/lists/create/").view_name == "core:create-todo-list"
 
 
 def test_lists():
-    assert reverse("core:lists-view") == "/app/lists"
-    assert resolve("/app/lists").view_name == "core:lists-view"
+    assert reverse("core:lists-view") == "/v0/lists"
+    assert resolve("/v0/lists").view_name == "core:lists-view"
 
 
 def test_default_redirect():
-    assert reverse("core:master-list") == "/app/"
-    assert resolve("/app/").view_name == "core:master-list"
+    assert reverse("core:master-list") == "/v0/"
+    assert resolve("/v0/").view_name == "core:master-list"
