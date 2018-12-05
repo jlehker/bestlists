dca533bdface994e3ada919a7373edc407b7e735
Jean-Michel Lehker <jean-michel@lehker.net>
2018-12-05 00:58:22 -0800

    add template

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,56 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
+  <div id="app">
+    <v-app id="inspire">
+      <v-navigation-drawer
+        fixed
+        v-model="drawer"
+        app
       >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+        <v-list dense>
+          <v-list-tile @click="">
+            <v-list-tile-action>
+              <v-icon>home</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Home</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile @click="">
+            <v-list-tile-action>
+              <v-icon>contact_mail</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Contact</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar color="indigo" dark fixed app>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>Application</v-toolbar-title>
+      </v-toolbar>
+      <v-content>
+        <v-container fluid fill-height>
+          <v-layout
+            justify-center
+            align-center
+          >
+            <v-flex text-xs-center>
+              <v-tooltip left>
+                <v-btn icon large :href="source" target="_blank" slot="activator">
+                  <v-icon large>code</v-icon>
+                </v-btn>
+                <span>Source</span>
+              </v-tooltip>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+      <v-footer color="indigo" app inset>
+        <span class="white--text">&copy; 2018</span>
+      </v-footer>
+    </v-app>
+  </div>
 </template>
 
 <script>
@@ -29,7 +61,7 @@ export default {
   components: {
     HelloWorld
   },
-  data () {
+  data() {
     return {
       //
     }
