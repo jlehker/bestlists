9b5943b54d5f8edcea1be5f808052ae6e87fe450
Jean-Michel Lehker <jean-michel@lehker.net>
2018-12-05 21:38:54 -0800

    template

--- src/App.vue
+++ src/App.vue
@@ -3,41 +3,77 @@
     <v-app id="inspire">
       <v-navigation-drawer
         fixed
-        v-model="drawer"
+        clipped
+        class="grey lighten-4"
         app
+        v-model="drawer"
       >
-        <v-list dense>
-          <v-list-tile @click="">
-            <v-list-tile-action>
-              <v-icon>home</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>Home</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-          <v-list-tile @click="">
-            <v-list-tile-action>
-              <v-icon>contact_mail</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>Contact</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
+        <v-list
+          dense
+          class="grey lighten-4"
+        >
+          <template v-for="(item, i) in items">
+            <v-layout
+              row
+              v-if="item.heading"
+              align-center
+              :key="i"
+            >
+              <v-flex xs6>
+                <v-subheader v-if="item.heading">
+                  {{ item.heading }}
+                </v-subheader>
+              </v-flex>
+              <v-flex xs6 class="text-xs-right">
+                <v-btn small flat>edit</v-btn>
+              </v-flex>
+            </v-layout>
+            <v-divider
+              dark
+              v-else-if="item.divider"
+              class="my-3"
+              :key="i"
+            ></v-divider>
+            <v-list-tile
+              :key="i"
+              v-else
+              @click=""
+            >
+              <v-list-tile-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title class="grey--text">
+                  {{ item.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
         </v-list>
       </v-navigation-drawer>
-      <v-toolbar color="indigo" dark fixed app>
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar color="amber" app absolute clipped-left>
+        <v-toolbar-side-icon @click.native="drawer = !drawer"></v-toolbar-side-icon>
+        <span class="title ml-3 mr-5">todo<span class="text">voodoo</span></span>
+        <v-text-field
+          solo-inverted
+          flat
+          label="Search"
+          prepend-icon="search"
+        ></v-text-field>
+        <v-spacer></v-spacer>
       </v-toolbar>
       <v-content>
-        <v-container fluid fill-height>
-          <v-layout
-            justify-center
-            align-center
-          >
-            <v-flex text-xs-center>
-              <v-tooltip left>
-                <v-btn icon large :href="source" target="_blank" slot="activator">
+        <v-container fluid fill-height class="grey lighten-4">
+          <v-layout justify-center align-center>
+            <v-flex shrink>
+              <v-tooltip right>
+                <v-btn
+                  icon
+                  large
+                  :href="source"
+                  target="_blank"
+                  slot="activator"
+                >
                   <v-icon large>code</v-icon>
                 </v-btn>
                 <span>Source</span>
@@ -46,25 +82,49 @@
           </v-layout>
         </v-container>
       </v-content>
-      <v-footer color="indigo" app inset>
-        <span class="white--text">&copy; 2018</span>
-      </v-footer>
     </v-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
   },
-  data() {
-    return {
-      //
+  data: () => ({
+      drawer: null,
+      items: [
+        { icon: 'lightbulb_outline', text: 'Master List' },
+        { icon: 'touch_app', text: 'Configure/Create Lists' },
+        { divider: true },
+        { heading: 'Labels' },
+        { icon: 'add', text: 'Create new label' },
+        { divider: true },
+        { icon: 'archive', text: 'Archive' },
+        { icon: 'delete', text: 'Trash' },
+        { divider: true },
+        { icon: 'settings', text: 'Settings' },
+        { icon: 'chat_bubble', text: 'Trash' },
+        { icon: 'help', text: 'Help' },
+      ]
+    }),
+    props: {
+      source: String
     }
-  }
-}
+};
 </script>
+
+<style>
+#keep main .container {
+  height: 660px;
+}
+.navigation-drawer__border {
+  display: none;
+}
+.text {
+  font-weight: 400;
+}
+</style>
