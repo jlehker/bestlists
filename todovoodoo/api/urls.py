9d824e4aeaaced932ace762ee48fce6378d06d03
Jean-Michel Lehker <jean-michel@lehker.net>
2018-12-07 09:40:39 -0800

    add drf swagger

--- todovoodoo/api/urls.py
+++ todovoodoo/api/urls.py
@@ -1,11 +1,15 @@
+from django.urls import path
 from rest_framework import routers
+from rest_framework_swagger.views import get_swagger_view
 
 from .viewsets import TodoListViewSet, ListItemViewSet
 
+app_name = "api"
+
+schema_view = get_swagger_view(title="Todovoodoo API")
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r"lists", ListItemViewSet, basename="TodoList")
 router.register(r"items", TodoListViewSet, basename="ListItem")
 
-app_name = "api"
-urlpatterns = router.urls
+urlpatterns = [path("", schema_view)] + router.urls
