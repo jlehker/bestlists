efcc9c48b3d64a6fb12995816f30239169c65f0a
Jean-Michel Lehker <jean-michel@lehker.net>
2019-11-04 08:16:52 -0800

    fix file path

--- todovoodoo/core/models.py
+++ todovoodoo/core/models.py
@@ -135,8 +135,9 @@ class StationItem(TimeStampedModel):
 
 def get_file_path(instance, filename):
     ext = filename.split(".")[-1]
-    filename = "%s.%s" % (uuid.uuid4(), ext)
-    return os.path.join("photos/user_uploads/%Y/%m/%d/", filename)
+    # filename = "%s.%s" % (uuid.uuid4(), ext)
+    # return os.path.join("photos/user_uploads/%Y/%m/%d/", filename)
+    return f"photos/user_uploads/%Y/%m/%d/{uuid.uuid4()}.{ext}"
 
 
 class ReportEntry(TimeStampedModel):
